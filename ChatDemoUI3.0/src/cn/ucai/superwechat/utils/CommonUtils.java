e78d2f8fdf4df412e43cf9fb2efc9113b867bd0f
lybrooks <506119809@qq.com>
2016-11-04 18:24:53 +0800

    添加消息码以及显示的方法

--- ChatDemoUI3.0/src/cn/ucai/superwechat/utils/CommonUtils.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/CommonUtils.java
@@ -2,20 +2,31 @@ package cn.ucai.superwechat.utils;
 
 import android.widget.Toast;
 
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWechatApplication;
 
 
 public class CommonUtils {
     public static void showLongToast(String msg){
-        Toast.makeText(SuperWechatApplication.getInstance(),msg,Toast.LENGTH_LONG).show();
+        Toast.makeText(SuperWechatApplication.applicationContext,msg,Toast.LENGTH_LONG).show();
     }
     public static void showShortToast(String msg){
-        Toast.makeText(SuperWechatApplication.getInstance(),msg,Toast.LENGTH_SHORT).show();
+        Toast.makeText(SuperWechatApplication.applicationContext,msg,Toast.LENGTH_SHORT).show();
     }
     public static void showLongToast(int rId){
-        showLongToast(SuperWechatApplication.getInstance().getString(rId));
+        showLongToast(SuperWechatApplication.applicationContext.getString(rId));
     }
     public static void showShortToast(int rId){
-        showShortToast(SuperWechatApplication.getInstance().getString(rId));
+        showShortToast(SuperWechatApplication.applicationContext.getString(rId));
+    }
+    public static void  showMsgShortToast(int msgId){
+        if(msgId>0){
+            showShortToast(SuperWechatApplication.getInstance().getResources()
+                    .getIdentifier(I.MSG_PREFIX_MSG,"string",SuperWechatApplication.getInstance().getPackageName()));
+        }else {
+            showShortToast(R.string.msg_1);
+        }
+
     }
 }
