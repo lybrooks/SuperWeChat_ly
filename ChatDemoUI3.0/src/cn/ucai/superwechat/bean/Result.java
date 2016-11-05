1ee94f8eaf6a23ceafdb0a4ca2521735c9d9151e
lybrooks <506119809@qq.com>
2016-11-05 10:52:00 +0800

    添加实体类

--- ChatDemoUI3.0/src/cn/ucai/superwechat/bean/Result.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/bean/Result.java
@@ -2,57 +2,42 @@ package cn.ucai.superwechat.bean;
 
 import java.io.Serializable;
 
-/**
- * Created by Administrator on 2016/10/13.
- */
 public class Result implements Serializable {
-
-    /**
-     * retCode : 0
-     * retMsg : true
-     * retData : {}
-     */
-
-    private int retCode;
-    private boolean retMsg;
-    private Object retData;
-
-    public int getRetCode() {
-        return retCode;
-    }
-
-    public void setRetCode(int retCode) {
-        this.retCode = retCode;
-    }
-
-    public boolean isRetMsg() {
-        return retMsg;
-    }
-
-    public void setRetMsg(boolean retMsg) {
-        this.retMsg = retMsg;
-    }
-
-    public Object getRetData() {
-        return retData;
-    }
-
-    public void setRetData(Object retData) {
-        this.retData = retData;
-    }
-
-    public static class RetDataBean {
-    }
-
-    public Result() {
-    }
-
-    @Override
-    public String toString() {
-        return "Result{" +
-                "retCode=" + retCode +
-                ", retMsg=" + retMsg +
-                ", retData=" + retData +
-                '}';
-    }
+	private int retCode = -1;
+	private boolean retMsg;
+	private Object retData;
+	public Result() {
+	}
+	public Result(boolean retMsg,int retCode){
+		this.retMsg = retMsg;
+		this.retCode = retCode;
+	}
+	public Result(int retCode, boolean retMsg, Object retData) {
+		super();
+		this.retCode = retCode;
+		this.retMsg = retMsg;
+		this.retData = retData;
+	}
+	public int getRetCode() {
+		return retCode;
+	}
+	public void setRetCode(int retCode) {
+		this.retCode = retCode;
+	}
+	public boolean isRetMsg() {
+		return retMsg;
+	}
+	public void setRetMsg(boolean retMsg) {
+		this.retMsg = retMsg;
+	}
+	public Object getRetData() {
+		return retData;
+	}
+	public void setRetData(Object retData) {
+		this.retData = retData;
+	}
+	@Override
+	public String toString() {
+		return "Result [retCode=" + retCode + ", retMsg=" + retMsg + ", retData=" + retData + "]";
+	}
 }
