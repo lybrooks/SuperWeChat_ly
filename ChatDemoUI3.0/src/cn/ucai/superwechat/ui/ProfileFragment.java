22e16bf6c14ace3346b768736b4ec34e70f254ff
lybrooks <506119809@qq.com>
2016-11-05 19:14:50 +0800

    添加个人中心布局

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ProfileFragment.java
@@ -0,0 +1,27 @@
+package cn.ucai.superwechat.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.ucai.superwechat.R;
+
+/**
+ * Created by Administrator on 2016/11/5.
+ */
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null && savedInstanceState.getBoolean("isConflict", false))
+            return;
+    }
+}
